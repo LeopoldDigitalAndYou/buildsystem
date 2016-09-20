10675b13c0d50c5818ad229ba22248c412777b9d
LeopoldDigitalAndYou <leo@garage56.fr>
2016-09-20 14:19:21 +0200

    webpack runs! sort of

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index.jade",
 	output: {
 		path: "./src",
-		filename: "[name].html"
+		filename: "index.html"
 	},
 	module: {
 		loaders: [
-			{test: ".jade", loader: "jade-loader"}
+			{test: /\.jade$/, loader: "jade"}
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin("style.css")
+	]
 };
