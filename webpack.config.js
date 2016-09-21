be414b400b5ccb6f77d930f80dd5854dbd9700da
LeopoldDigitalAndYou <leo@garage56.fr>
2016-09-21 10:24:51 +0200

    Digital & You page

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,21 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	entry: ["./src/style/redthings.scss", "./src/style/bluethings.scss",
-		"./src/js/hello.js", "./src/js/42.js"],
+	entry: ["./src/style/index.scss",
+		'./src/js/startFoundation.js'],
 	output: {
 		path: "./dist",
 		filename: "bundle.js"
 	},
 	module: {
 		loaders: [
-			{test: /\.js$/, loader: "babel-loader?presets[]=es2015"},
+			//{test: /\.js$/, loader: "babel-loader?presets[]=es2015"},
 			{test: /\.jade$/, loader: "jade"},
 			{test: /\.scss$/, loader: ExtractTextPlugin.extract("css!sass")}
 		]
 	},
 	plugins: [
-		new ExtractTextPlugin("bluethings.css"),
-		new ExtractTextPlugin("redthings.css"),
+		new ExtractTextPlugin("index.css"),
 		new HtmlWebpackPlugin({filename: "index.html", template:"./src/index.jade"})
 	]
 };
